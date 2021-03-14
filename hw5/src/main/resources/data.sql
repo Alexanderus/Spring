05db0ff9e7cd33d225c19358c0dced8532641e0a
Aleksandr Ostapenko <AVlOstapenko@sberbank.ru>
2021-03-14 20:27:51 +0300

    home work 5

---
+++ hw5/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO BOOKS (author, title, genre) VALUES
+    ('Joshua Bloch', 'Effective Java', 'programming'),
+    ('Joshua Bloch', 'Java Puzzlers', 'programming'),
+    ('Herbert Schildt', 'Java: The Complete Reference, Eleventh Edition', 'programming'),
+    ('Daniel Liang', 'Introduction to Java Programming and Data Structures', 'programming'),
+    ('Birdie Pinedo', 'Quantum Physics In Brief', 'science'),
+    ('David Halliday', 'Fundamentals of Physics', 'science'),
+    ('Barbara Neal', 'When We Believed in Mermaids', 'novel'),
+    ('Rachel Caine', 'Stillhouse Lake', 'novel'),
+    ('Rachel Caine', 'Wolfhunter River', 'novel'),
+    ('Douglas C. Giancoli', 'Physics: Principles with Applications', 'science');
